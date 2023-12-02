6f8297fb4c5c4943dbf8fa8ef8a22d2406939610
Spiss, Lukas <lukas@spiss.dev>
2023-12-02 09:40:48 +0000

    Solve day2/puzzle2

--- day2/puzzle_2.go
+++ day2/puzzle_2.go
@@ -1,1 +1,31 @@
 package day2
+
+func solvePuzzle2(game map[int]Game) int {
+	power := 0
+
+	for _, v := range game {
+		minR := 0
+		minG := 0
+		minB := 0
+
+		for _, round := range v.rounds {
+			r := round.red
+			g := round.green
+			b := round.blue
+
+			if r > minR {
+				minR = r
+			}
+			if g > minG {
+				minG = g
+			}
+			if b > minB {
+				minB = b
+			}
+		}
+
+		power += minR * minG * minB
+	}
+
+	return power
+}
--- day2/puzzle_2_test.go
+++ day2/puzzle_2_test.go
@@ -1,1 +1,17 @@
 package day2
+
+import (
+	"aoc-2023/util"
+	"testing"
+)
+
+func TestSolvePuzzle2(t *testing.T) {
+	input := util.ReadInput(2)
+
+	game := parseGame(input)
+	result := solvePuzzle2(game)
+
+	if result != 78375 {
+		t.Error(result)
+	}
+}
